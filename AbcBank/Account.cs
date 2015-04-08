e406d90de0cd282d32921728692b3cf95c9b8439
git-hubris <staylogical@hotmail.com>
2015-04-08 09:35:26 -0400

    Update Account.cs

--- AbcBank/Account.cs
+++ AbcBank/Account.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace AbcBank
 {
@@ -48,39 +49,17 @@ namespace AbcBank
 
         public double interestEarned()
         {
-            double amount = sumTransactions();
-            switch (accountType)
             {
-                case SAVINGS:
-                    if (amount <= 1000)
-                        return amount * 0.001;
-                    else
-                        return 1 + (amount - 1000) * 0.002;
-                // case SUPER_SAVINGS:
-                //     if (amount <= 4000)
-                //         return 20;
-                case MAXI_SAVINGS:
-                    if (amount <= 1000)
-                        return amount * 0.02;
-                    if (amount <= 2000)
-                        return 20 + (amount - 1000) * 0.05;
-                    return 70 + (amount - 2000) * 0.1;
-                default:
-                    return amount * 0.001;
+                double amount = SumTransactions();
+                return (amount * 0.001)/365;
             }
         }
-
-        public double sumTransactions()
-        {
-            return checkIfTransactionsExist(true);
-        }
-
-        private double checkIfTransactionsExist(bool checkAll)
+        public double SumTransactions()
         {
             double amount = 0.0;
             foreach (Transaction t in transactions)
                 amount += t.amount;
-            return amount;
+            return amount * 0.01;
         }
 
         public int getAccountType()

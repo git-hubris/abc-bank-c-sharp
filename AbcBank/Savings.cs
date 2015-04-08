d3063a1eaa7b5205c179d3886fb9a7d91ae7c97f
git-hubris <staylogical@hotmail.com>
2015-04-08 09:34:46 -0400

    Create Savings.cs

---
+++ AbcBank/Savings.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbcBank
+{
+    public class Savings : Account
+    {
+        public override double InterestEarned()
+        {
+            double amount = SumTransactions();
+            if (amount <= 1000)
+                return (amount * 0.001)/365;
+            else
+                return (1 + (amount - 1000) * 0.002)/365;
+        }
+    }
+}
